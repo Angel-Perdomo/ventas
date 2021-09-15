6cf833de0475d3f5ecfb00611b7047afb85369e7
Angel-Perdomo <pastorangel1880@gmail.com>
2021-09-14 18:10:11 -0600

    Proyecto Mensaje motivacional

---
+++ WinFormsMensajeMotiv/WinFormsMensajeMotiv/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsMensajeMotiv
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_procesar_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("La imaginación es más importante que el conocimiento, el conocimiento es limitado, la imaginación rodea al mundo.");
+        }
+    }
+}
